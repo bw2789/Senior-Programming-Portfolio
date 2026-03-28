fecc13833a85f607c8e8c7698d96a169ea6e560e
bw2789 <bojunw2789@nycstudents.net>
2026-03-28 21:17:02 +0000

    added split text to #Senior-Programming-Portfolio where the text have a onload aniamtion that the text pops up

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ gsap.to(".creative-text span", {
     markers: true
   }
 });
+
+gsap.registerPlugin(SplitText) 
+
+let split = SplitText.create("#Senior-Programming-Portfolio", { type: "chars" });
+let chars = split.chars
+
+gsap.from(chars, {
+  yPercent: 130,
+  stagger: 0.02,
+  ease: "back.out",
+  duration: 0.5
+
+
+})
