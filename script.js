42929f95081f35d3f8f1cfdcb2ce713daaa9c5a4
bw2789 <bojunw2789@nycstudents.net>
2026-03-28 22:58:15 +0000

    need to change the id for senior .... beacuse i changed the text and its kinda miss leading

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".creative-text span", {
-  yPercent: 50,  // relative to CSS transform
-  opacity: 1,
-  rotation: gsap.utils.random(-10, 10),
-  ease: "power2.out",
-  duration: 1.5,
-  stagger: 0.05,
-  scrollTrigger: {
-    trigger: ".creative-text",
-    start: "top 90%",
-    end: "bottom 50%",
-    scrub: 0.6,
-    markers: true
-  }
-});
+
 
 gsap.registerPlugin(SplitText) 
 
