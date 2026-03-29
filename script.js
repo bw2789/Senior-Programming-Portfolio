817b96b468c6b760e9dc42e688b8f6fd8ac3bab3
bw2789 <bojunw2789@nycstudents.net>
2026-03-29 03:08:22 +0000

    add animation for pfp, the same animation as the text

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
-
-
-
-gsap.registerPlugin(SplitText) 
+gsap.registerPlugin(SplitText);
 
 let split = SplitText.create("#Senior-Programming-Portfolio", { type: "chars" });
 let chars = split.chars
 
-gsap.from(chars, {
+
+let tl = gsap.timeline();
+
+tl.from(chars, {
   yPercent: 130,
+  opacity: 0,
   stagger: 0.02,
   ease: "back.out",
   duration: 0.5
-
-
 })
+.from("#pfp", {
+  y: 100,         
+  opacity: 0,      
+  scale: 0.8,     
+  duration: 0.8,
+  ease: "back.out(1.7)"
+}, "<"); // "<" means start at the same time as previous animation
+
